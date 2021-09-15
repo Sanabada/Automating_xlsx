53d2d1e4a8d2d09103e1604bd37d63e54c1aaf2d
Abhinita Sanabada <90692454+Sanabada@users.noreply.github.com>
2021-09-15 19:37:39 +0530

    Add files via upload
    Adding codes for automation project

---
+++ duplicate.py
@@ -0,0 +1,11 @@
+duplicates = [10,10,20]
+unique = []
+for i in duplicates:
+    if i not in unique:
+        unique.append(i)
+print(unique)
+
+
+
+
+
---
+++ formater.py
@@ -0,0 +1,6 @@
+first = input("First name :")
+last = input("Last name: ")
+msg= f'{first} [{last}] is a coder.'
+print(msg)
+check = 'testing the program'
+print(check.title())
